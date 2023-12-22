15f46101cfde9eb9a7b568638f14a769b4449cc6
William Bohrmann <wbohrmann@esri.com>
2023-12-22 11:33:01 -0800

    Reference maui community toolkit

--- TapGestureRecognizerBug/MauiProgram.cs
+++ TapGestureRecognizerBug/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
 namespace TapGestureRecognizerBug
 {
@@ -9,6 +10,7 @@ namespace TapGestureRecognizerBug
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
